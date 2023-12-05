4ada22ed219a7512eae67c5447a42440d826fc30
HamaHs <lusuy1995@gmail.com>
2023-12-05 23:55:43 +0100

    add 2 new components

---
+++ 08-reusable-components/src/components/MyComponent.js
@@ -0,0 +1,10 @@
+const MyComponent = function () {
+   return (
+      <div>
+         <h1>Hello from reusable component</h1>
+         <button>Like!</button>
+      </div>
+   );
+};
+
+export default MyComponent;
