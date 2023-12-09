2014b3a09cc9423a9f27499c5609257305278c2e
HamaHs <lusuy1995@gmail.com>
2023-12-09 12:06:49 +0100

    add comment

--- 12-state-via-props/src/App.js
+++ 12-state-via-props/src/App.js
@@ -6,6 +6,7 @@ import Counter from "./components/Counter";
 function App() {
    const [countNum, setCountNum] = useState(0);
 
+   // краще передати incrementCountNum() функцію кнопці, ніж передавати setCountNum() разом з countNum, тому що компоненту Button не потрібно знати яке число на разі є, ця інформація для Button буде ізбиточна.
    const incrementCountNum = () => {
       setCountNum(countNum + 1);
    };
