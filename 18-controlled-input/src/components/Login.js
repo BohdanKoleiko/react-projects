37b37cea359c5db1b06ec2671833ad77af851c3c
HamaHs <lusuy1995@gmail.com>
2023-12-14 18:46:29 +0100

    use object in useState of component

--- 18-controlled-input/src/components/Login.js
+++ 18-controlled-input/src/components/Login.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 const Login = function () {
+   const [data, setData] = useState({ username: "", password: "" });
+
    const hundlerFormSubmit = function (event) {
       event.preventDefault();
-      const userData = {
-         username: event.target.username.value,
-         password: event.target.password.value,
-      };
 
-      console.log();
-      alert(JSON.stringify(userData));
+      console.log(data);
+      alert(JSON.stringify(data));
+   };
+
+   const hundleInputChange = function (text, name) {
+      setData({ ...data, [name]: text });
    };
 
    return (
@@ -16,11 +20,23 @@ const Login = function () {
          <form onSubmit={hundlerFormSubmit}>
             <label>
                Username:
-               <input type="text" name="username" />
+               <input
+                  type="text"
+                  value={data.username}
+                  onChange={(e) => {
+                     hundleInputChange(e.target.value, "username");
+                  }}
+               />
             </label>
             <label>
                Password:
-               <input type="password" name="password" />
+               <input
+                  type="password"
+                  value={data.password}
+                  onChange={(e) => {
+                     hundleInputChange(e.target.value, "password");
+                  }}
+               />
             </label>
             <button type="submit">Login</button>
          </form>
