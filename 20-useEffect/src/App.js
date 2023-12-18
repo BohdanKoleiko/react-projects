0779f8cb34530ab500658923a303f824b25bb76d
HamaHs <lusuy1995@gmail.com>
2023-12-18 17:55:58 +0100

    practice to use useEffect

--- 20-useEffect/src/App.js
+++ 20-useEffect/src/App.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-   return <div className="App"></div>;
+   const [todo, setTodo] = useState(null);
+
+   useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+         .then((response) => response.json())
+         .then((json) => setTodo(json));
+   }, []);
+
+   const parsTodo = function (todos) {
+      const tasks = [];
+
+      for (let i = 0; i < todos.length; i++) {
+         if (i < 10) {
+            tasks.push(todos[i]);
+         }
+      }
+
+      return tasks;
+   };
+
+   //console.log("App rendered");
+   //console.log(todo);
+
+   return (
+      <div className="App">
+         {todo &&
+            parsTodo(todo).map((task) => {
+               return <h1 key={task.id}>{task.title}</h1>;
+            })}
+      </div>
+   );
 }
 
 export default App;
